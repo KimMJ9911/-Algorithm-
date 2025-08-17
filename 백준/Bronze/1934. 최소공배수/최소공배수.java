8bade040e2582dae9011d75bec7222544b45e7ca
KimMJ9911 <verox0304@gmail.com>
2025-08-17 14:20:00 +0900

    [Bronze I] Title: 최소공배수, Time: 140 ms, Memory: 16324 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine() , " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            int gcd = GCD(A , B);
+            int lcd = (A * B) / gcd;
+
+            bw.write(lcd + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    private static int GCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b  = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
