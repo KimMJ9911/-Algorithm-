4c6c26dbee5f08b17748742f5d93b9cb56a343a9
KimMJ9911 <verox0304@gmail.com>
2025-09-17 20:39:36 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 140 ms, Memory: 19176 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -2,20 +2,21 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n , max = Integer.MIN_VALUE , min = Integer.MAX_VALUE;
-    static int[] operator , nums;
+    static int n , min = Integer.MAX_VALUE , max = Integer.MIN_VALUE;
+    static int[] operator;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
-        nums = new int[n];
         operator = new int[4];
+        arr = new int[n];
 
         st = new StringTokenizer(br.readLine() , " ");
         for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine() , " ");
@@ -23,7 +24,7 @@ public class Main {
             operator[i] = Integer.parseInt(st.nextToken());
         }
 
-        DFS(nums[0] , 1);
+        set_calculate(arr[0] , 1);
 
         bw.write(max + "\n" + min);
         bw.flush();
@@ -31,36 +32,33 @@ public class Main {
         bw.close();
     }
 
-    private static void DFS(int init, int k) {
-        if (k == n) {
+    private static void set_calculate(int init , int depth) {
+        if (depth == n) {
             max = Math.max(max , init);
             min = Math.min(min , init);
             return;
         }
 
-        //연산자를 이용해 백트래킹 설정
-        //각 연산자가 하나씩 연산되는 경우 뒤로 돌아갈 재귀를 한 연산마다 설정
-        //이 연산이 종료된 후 다시 operator[k]++ 로 복귀
         for (int i = 0; i < 4; i++) {
             if (operator[i] != 0) {
                 operator[i]--;
-
                 switch (i) {
                     case 0:
-                        DFS(init + nums[k] , k + 1);
+                        set_calculate(init + arr[depth] , depth + 1);
                         break;
                     case 1:
-                        DFS(init - nums[k] , k + 1);
+                        set_calculate(init - arr[depth] , depth + 1);
                         break;
                     case 2:
-                        DFS(init * nums[k] , k + 1);
+                        set_calculate(init * arr[depth] , depth + 1);
                         break;
                     case 3:
-                        DFS(init / nums[k] , k + 1);
+                        set_calculate(init / arr[depth] , depth + 1);
+                        break;
                 }
-                //재귀가 종료되면 다시 roll back
                 operator[i]++;
             }
+
         }
     }
 }
