3915092b93a2b59ea5cad646b7e6e6c83bb8d79f
KimMJ9911 <verox0304@gmail.com>
2025-09-18 22:50:26 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 15792 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,26 +2,26 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited;
-    static int[] ans;
     static int n , m;
+    static int[] arr;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine() , " ");
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[n + 1];
-        ans = new int[m + 1];
+        arr = new int[m];
+        visited = new boolean[n];
 
         DFS(0 , 0);
     }
-    static void DFS(int depth , int k) {
+
+    private static void DFS(int depth , int k) {
         if (depth == m) {
             for (int i = 0; i < m; i++) {
-                System.out.print(ans[i] + " ");
+                System.out.print(arr[i] + " ");
             }
             System.out.println();
             return;
@@ -30,7 +30,7 @@ public class Main {
         for (int i = k; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                ans[depth] = i + 1;
+                arr[depth] = i + 1;
                 DFS(depth + 1 , i + 1);
                 visited[i] = false;
             }
