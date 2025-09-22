50b9c5976f852667b07c2053339af33b9461e077
KimMJ9911 <verox0304@gmail.com>
2025-09-22 17:35:15 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -4,27 +4,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-        long[] dp = new long[101];
-        for (int j = 0; j < T; j++) {
+        for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
+            long[] arr = new long[n + 1];
 
-
-            if (n <= 3) bw.write(1 + "\n");
+            if (n == 1 || n == 2 || n == 3) sb.append(1).append("\n");
+            else if (n == 4 || n == 5) sb.append(2).append("\n");
             else {
-                dp[1] = 1;
-                dp[2] = 1;
-                dp[3] = 1;
-
-                for (int i = 4; i <= n; i++) {
-                    dp[i] = dp[i - 2] + dp[i - 3];
+                arr[1] = 1;
+                arr[2] = 1;
+                arr[3] = 1;
+                arr[4] = 2;
+                arr[5] = 2;
+                for (int j = 6; j <= n; j++) {
+                    arr[j] = arr[j - 5] + arr[j - 1];
                 }
-                bw.write(dp[n] + "\n");
+                sb.append(arr[n]).append("\n");
             }
         }
 
+        bw.write(String.valueOf(sb));
         bw.flush();
         br.close();
         bw.close();
