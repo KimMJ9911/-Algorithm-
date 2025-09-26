4260411e42db834419edce2d4e845c3458d4d79d
KimMJ9911 <verox0304@gmail.com>
2025-09-26 23:31:58 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 123 ms, Memory: 30,572 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,13 +1,15 @@
 import java.io.*;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Solution {
     static class Node implements Comparable<Node> {
-       int c , w , r;
-        public Node(int c , int w , int r) {
-            this.c = c;
-            this.w = w;
+        int x , y , r;
+
+        public Node(int x , int y , int r) {
             this.r = r;
+            this.x = x;
+            this.y = y;
         }
 
         @Override
@@ -15,70 +17,70 @@ public class Solution {
             return Integer.compare(this.r , o.r);
         }
     }
-
-    static int[][] map , distance;
+    static int[] vert = new int[] {0 , 0 , -1 , 1} , horiz = new int[]{-1 , 1 , 0 , 0};
+    static int[][] distance , table;    //  필요한 총 거리를 저장하는 배열
     static boolean[][] visited;
-    static PriorityQueue<Node> pq;
     static int n;
-    static int[] dc = new int[] {0 , 0 , 1 , -1};
-    static int[] dw = new int[] {1 , -1 , 0 , 0};
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        //상하좌우 움직이면서 경우에 따라 이동하는 과정이 필요
+        StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
         for (int i = 1; i <= T; i++) {
             n = Integer.parseInt(br.readLine());
-            map = new int[n][n];
-            visited = new boolean[n][n];
+            table = new int[n][n];
             distance = new int[n][n];
+            visited = new boolean[n][n];
 
             for (int j = 0; j < n; j++) {
                 String line = br.readLine();
                 for (int k = 0; k < n; k++) {
-                    map[j][k] = line.charAt(k) - '0';
+                    table[j][k] = line.charAt(k) - '0';
                     distance[j][k] = Integer.MAX_VALUE;
                 }
             }
+
             Dijkstra(i);
         }
 
-        bw.write(sb + "");
+        bw.write(String.valueOf(sb));
         bw.flush();
         br.close();
         bw.close();
     }
 
-    private static void Dijkstra(int i) {
+    static PriorityQueue<Node> pq;
+    private static void Dijkstra(int idx) {
         pq = new PriorityQueue<>();
-        pq.offer(new Node(0 , 0 , 0));
-
-        //초기 값 설정
+        pq.offer(new Node(0 , 0 , 0));  //  시작 지점
         distance[0][0] = 0;
 
         while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            if (node.c == n - 1 && node.w == n - 1) {
-                sb.append("#").append(i).append(" ").append(distance[n - 1][n - 1]).append("\n");
+            Node next = pq.poll();
+            if (next.x == n - 1 && next.y == n - 1) {
+                sb.append("#").append(idx).append(" ").append(distance[n - 1][n - 1]).append("\n");
                 break;
             }
 
-            if (!visited[node.c][node.w]) visited[node.c][node.w] = true;
+            if (!visited[next.x][next.y])
+                visited[next.x][next.y] = true;
 
-            //4 방향 전체 검사
-            for (int j = 0; j < 4; j++) {
-                int nc = node.c + dc[j];
-                int nw = node.w + dw[j];
+            for (int i = 0; i < 4; i++) {
+                int nx = next.x + horiz[i];
+                int ny = next.y + vert[i];
 
-                if (isValid(nc , nw) && !visited[nc][nw] && node.r + map[nc][nw] < distance[nc][nw]) {
-                    distance[nc][nw] = node.r + map[nc][nw];
-                    pq.offer(new Node(nc , nw , distance[nc][nw]));
+                if (isValid(nx , ny) && !visited[nx][ny] && next.r + table[nx][ny] < distance[nx][ny]) {
+                    distance[nx][ny] = next.r + table[nx][ny];
+                    pq.offer(new Node(nx , ny , distance[nx][ny]));
                 }
             }
         }
     }
-    static boolean isValid(int nc , int nw) {
-        return 0 <= nc && nc < n && 0 <= nw && nw < n;
+
+    private static boolean isValid(int nx , int ny) {
+        return 0 <= nx && nx < n && 0 <= ny && ny < n;
     }
 }
