1c506cf7b4e79421a3b0432031eb7fd59ac1e067
KimMJ9911 <verox0304@gmail.com>
2025-10-18 23:54:34 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    unsigned long long n;
+    cin >> n;
+    cout << n * n << "\n" << 2 << std::endl;
+}
