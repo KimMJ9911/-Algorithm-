ed87f4e1f21fcd2c872240b4d352ce2d955c5154
KimMJ9911 <verox0304@gmail.com>
2025-10-28 13:03:13 +0900

    [Silver IV] Title: 가로수, Time: 256 ms, Memory: 28468 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -1,51 +1,46 @@
-import java.util.*;
-
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String... args) {
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> tree = new ArrayList<>();
 
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-
-        List<Integer> list     = new ArrayList<>();
-        List<Integer> distance = new ArrayList<>();
-
-        while (N-- > 0) {
-            list.add(sc.nextInt());
+        //가로수 전체 받기
+        for (int i = 0; i < n; i++) {
+            tree.add(Integer.parseInt(br.readLine()));
         }
 
-        list.sort(Comparator.naturalOrder());
-
-        for (int i = 0; i < list.size() - 1; i++) {
-            distance.add(list.get(i + 1) - list.get(i));
+        //가로수 간의 간격 받기
+        List<Integer> div = new ArrayList<>();
+        for (int i = 0; i < n - 1; i++) {
+            div.add(tree.get(i + 1) - tree.get(i));
         }
 
-        int GCD = distance.get(0);
-        int sum = 0;
-
-        for (int i = 0; i < distance.size(); i++) {
-            GCD = euclidean(GCD, distance.get(i));
+        int gcd = GCD(div.get(0) , div.get(1));
+        for (int i = 1; i < n - 2; i++) {
+            gcd = GCD(gcd , GCD(div.get(i + 1) , div.get(i)));
         }
 
-        for (int d : distance) {
-            sum += ((d / GCD) - 1);
-        }
-
-        System.out.println(sum);
-
-        sc.close();
+        bw.write(String.valueOf((tree.get(n - 1) - tree.get(0)) / gcd - n + 1));
+        bw.flush();
+        br.close();
+        bw.close();
     }
 
-    // a = bq + r 유클리드 호제법
-    public static int euclidean(int A, int B) {
+    private static int GCD(int a, int b) {
+        int val = 0;
+        do {
+            val = a % b;
+            a = b;
+            b = val;
+        } while (val != 0);
 
-        if (B == 0) {
-            return A;
-        }
-        else {
-            return euclidean(B, A % B);
-        }
+        return a;
     }
 }
