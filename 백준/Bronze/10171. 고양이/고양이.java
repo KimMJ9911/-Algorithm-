7e3394fb1cbd48776dbffc85309cd34c3dddb6a7
KimMJ9911 <verox0304@gmail.com>
2025-10-29 09:00:11 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+
+        //bw.flush();
+        br.close();
+        bw.close();
+    }
+}
