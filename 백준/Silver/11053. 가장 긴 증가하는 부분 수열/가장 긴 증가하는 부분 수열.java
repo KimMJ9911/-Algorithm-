b5fe38c94808e5392238c4d8379da70280b68192
KimMJ9911 <verox0304@gmail.com>
2025-11-21 13:18:00 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14720 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,38 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
-    static int n;
-    static int[] dp , arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        dp = new int[n];
-
-        st = new StringTokenizer(br.readLine() , " ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 0; i < n; i++) {
-            dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[j] + 1 , dp[i]);
-                }
-            }
-        }
-
-        int max = IntStream.of(dp).max().orElse(0);
-        bw.write(String.valueOf(max));
-        bw.flush();
-        br.close();
-        bw.close();
-    }
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int max = 1;
+		
+		st = new StringTokenizer(br.readLine() , " ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] dp = new int[n];
+		for (int i = 0; i < n; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) dp[i] = Math.max(dp[j] + 1 ,  dp[i]);
+			}
+		}
+		
+		bw.write(String.valueOf(Arrays.stream(dp).max().getAsInt()));
+		bw.flush();
+		br.close();
+		bw.close();
+	}
 }
