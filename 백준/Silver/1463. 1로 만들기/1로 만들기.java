7e466a259accaa05a2bff70e92596b463cc0135c
KimMJ9911 <verox0304@gmail.com>
2025-11-21 13:36:25 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18252 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,32 +1,33 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-
-        int[] dp = new int[1000001];
-
-        dp[2] = 1;
-        dp[3] = 1;
-
-        for (int i = 4; i <= n; i++) {
-            //새로운 값 갱신을 위해 cnt에 dp값 저장
-            //이전 값에 저장된 dp 값  + 1
-            int cnt = dp[i - 1] + 1;
-            //3으로 나뉘어지는 경우 -> 3으로 나눈 값의 dp 값 + 1
-            if (i % 3 == 0) cnt = Math.min(dp[i / 3] + 1 , cnt);
-            //2으로 나뉘어지는 경우 -> 2으로 나눈 값의 dp 값 + 1
-            if (i % 2 == 0) cnt = Math.min(dp[i / 2] + 1 , cnt);
-
-            //새로운 값 dp에 저장
-            dp[i] = cnt;
-        }
-
-        bw.write(dp[n] + "");
-        bw.flush();
-        br.close();
-        bw.close();
-    }
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[1000001];
+		
+		dp[2] = 1;
+		dp[3] = 1;
+		for (int i = 4; i <= n; i++) {
+			int cnt = dp[i - 1] + 1;
+			if (i % 3 == 0) cnt = Math.min(dp[i / 3] + 1, cnt);
+			if (i % 2 == 0) cnt = Math.min(dp[i / 2] + 1, cnt);
+			
+			dp[i] = cnt;
+		}
+		
+		bw.write(String.valueOf(dp[n]));
+		bw.flush();
+		br.close();
+		bw.close();
+	}
 }
